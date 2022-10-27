1bf20af0abdb2fc3463992a3c0a400a9162d487f
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2022-10-27 10:30:28 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-var score = 0;
-
-document.getElementById("sumarpto");
-
-
-
-function puntaje(){
-    result = score ++;
-    if(result == 1){
-        document.getElementById('sumarpto').innerHTML='Un dulce de mil pesos';
-    }else if(result == 5){
-        document.getElementById('sumarpto').innerHTML='Un dulce de 5 mil pesos';
-    }else if(result == 10){
-        document.getElementById('sumarpto').innerHTML='Un dulce de 10 mil pesos';
-    }else if(result == 15){
-        document.getElementById('sumarpto').innerHTML='Un dulce de 15 mil pesos';
-    }
-}
-
-
-
 // jquery para modal eliminar participante
 $('#eliminar').on('show.bs.modal', function(e){
     $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
